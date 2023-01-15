7896b7c945149a6cdf84481996b99f88b0a55d5c
liminghao0914 <liminghao0914@gmail.com>
2023-01-15 23:03:45 +0800

    update eva_plot

--- algorithm/eva_plot.py
+++ algorithm/eva_plot.py
@@ -5,20 +5,29 @@ from STSP import x_seq, n_method, LSTM_data
 from markov_model import markov_model, markov_data
 from svm_model import svm_data
 
+plt.rcParams['text.usetex'] = True
+
 pred_step = 30
 test_num = 1000
 
-# x_lstm, y_lstm= LSTM_data(x_seq, n_method, test_num, pred_step)
-# x_lstm, y_lstm= markov_data(markov_model, x_seq, test_num, pred_step)
+x_lstm, y_lstm= LSTM_data(x_seq, n_method, test_num, pred_step)
 x_markov, y_markov = markov_data(markov_model, x_seq, test_num, pred_step)
 x_svm, y_svm = svm_data(x_seq, test_num, pred_step)
+# save the data
+np.save('x_lstm.npy', x_lstm)
+np.save('y_lstm.npy', y_lstm)
+np.save('x_markov.npy', x_markov)
+np.save('y_markov.npy', y_markov)
+np.save('x_svm.npy', x_svm)
+np.save('y_svm.npy', y_svm)
 
-def comparison(y_lstm, y_markov):
+def comparison(y_lstm, y_markov, y_svm):
   x_axis = np.arange(1, pred_step+1)
-  plt.plot(x_axis, y_lstm, color='blue' ,label='LSTM')
-  plt.plot(x_axis, y_markov, color='green', label='Markov')
-  plt.xlabel('Prediction step')
-  plt.ylabel('Accuracy')
+  plt.plot(x_axis, y_lstm, color='red' ,label='LSTM')
+  plt.plot(x_axis, y_markov, color='blue' ,label='Markov')
+  plt.plot(x_axis, y_svm, color='green', label='SVM')
+  plt.xlabel(r'Number of Prediction Step ($n$)')
+  plt.ylabel(r'Accuracy')
   plt.legend()
   plt.show()
 
